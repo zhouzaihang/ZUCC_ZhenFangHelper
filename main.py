ba93fd0c5514029ea4a5727161946bd3f0b37eb6
Zaihang <1085212484@qq.com>
2018-11-17 10:45:38 +0800

    修改Main逻辑，完善异常处理

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
+import requests
 import Login as Lg
 import PlannedCourseSpider
 import PublicCourseSpider
 
-
-def run(choose):
-    if choose == 0:
-        spider = PublicCourseSpider.PublicLessonSpider(number, password, name)
-        spider.run()
-    elif choose == 1:
-        spider = PlannedCourseSpider.PlannedCourseSpider(number, password, name)
-        spider.run()
-
-
-number, password, name = Lg.get_information()
 choose_id = ''
-
 while True:
     choose_id = input("输入0进入公选课选课，输入1进入计划内课程选课")
     if choose_id.isdigit():
@@ -24,7 +13,19 @@ while True:
 
 while True:
     try:
-        run(choose_id)
-    except Exception:
+        number, password, name = Lg.get_information()
+        spider = ''
+        if choose_id == 0:
+            spider = PublicCourseSpider.PublicLessonSpider(number, password, name)
+        elif choose_id == 1:
+            spider = PlannedCourseSpider.PlannedCourseSpider(number, password, name)
+        spider.run()
+    except requests.exceptions.ConnectionError:
         print("Error: 网络好像有点问题")
         continue
+    except FileNotFoundError:
+        print("文件读取异常，检查字模和information.json")
+        break
+    except KeyboardInterrupt:
+        print("结束")
+        break
